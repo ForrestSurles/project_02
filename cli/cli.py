13bfc47e72306ed783a0e6d4563f7e9b8420fa75
Forrest Surles <85535986+ForrestSurles@users.noreply.github.com>
2021-10-10 23:45:16 -0500

    Add options to CLI

--- cli/cli.py
+++ cli/cli.py
@@ -3,28 +3,77 @@
 """
 
 import fire
-from questionary
+from questionary import text, prompt, Separator
+
+user_fail_msg = 'No input was recognized. Please try again.'
 
 def portfolio_modeler():
-    questions = [
+
+    # list of functions for main menu prompt
+    function_list = [
+        'Input Portfolio',
+        'Run Model',
+        Separator(),
+        'Exit'
+    ]
+
+    # ask the user for their name
+    user_name = text('Please enter your name, and then press [Enter].').ask()
+
+    # create default response
+    name_response = user_fail_msg
+
+    if user_name != '':
+        name_response = f'Welcome, {user_name}.'
+
+    # validate a response
+    if name_response != user_fail_msg:
+        print(name_response)
+    else:
+        print(f'{user_fail_msg}\nNow Exiting...')
+        exit()
+
+    options = [
         {
-            'type': 'confirm',
-            'name': 'no_user_input',
-            'message': 'No input was recognized. Please try again.',
-            'default': True
+            'type': 'select',
+            'name': 'main_menu',
+            'message': 'What would you like to do?',
+            'choices': function_list,
         },
         {
             'type': 'text',
-            'name': 'user_name',
-            'message': 'Please enter your name, and then press [Enter].',
-            # validate user input
-            'when': lambda x: x['no_user_input'],
-            # only accept non-empty string
-            'validate': lambda val: val != ''
+            'name': 'portfolio_input',
+            'message': 'Enter the portfolio tickers',
+            'multiline': True,
+            'instruction': \
+                'one ticker per line, press [Escape] then [Enter] to finish.'
         }
     ]
 
-        # Welcome the user, or indicate no name provided
+    # call the menu
+    menu_prompt = prompt(options)
+
+    # respond to user input
+    if menu_prompt['main_menu'] == function_list[-1] \
+        and menu_prompt['are_u_suuure']:
+        
+        print('Have a good day.\nNow Exiting...')
+        exit()    
+    elif menu_prompt['main_menu'] == 'Input Portfolio':
+        portfolio_tickers = []
+        for ticker in menu_prompt['portfolio_input'].splitlines():
+            portfolio_tickers.append(ticker)
+        print(f'List of Tickers:\n{portfolio_tickers}')
+   elif menu_prompt['main_menu'] == 'Run Model':
+
+    else:
+        menu_prompt = prompt(options)
+    # input portfolio
+
+    # require portfolio to run model
+    # option to redirect to portfolio if none when run model/ if not, menu
+
+    exit()
 
 
 if __name__ == '__main__':
